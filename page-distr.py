0dda81001517bcc5fc53093cb02e35363058543a
Dmitriy Sharavyov <dimsharav@gmail.com>
2013-01-30 03:52:06 +0600

    Добавлена поддержка книжного распределения.

--- page-distr.py
+++ page-distr.py
@@ -20,37 +20,51 @@ import sys
 
 def parser():
     prog = 'page-distr.py'
-    usage = '%prog [Options] begin_page end_page'
+    usage = '%prog [options] begin_page end_page'
     version = '0.2'
     p = optparse.OptionParser(description=description,
                               prog=prog,
                               usage=usage,
                               version=version)
+    p.add_option('--type', '-t', type='choice', dest='type',
+                 help='Тип распределения: list или book. По-умолчанию: list',
+                 choices=['list', 'book'],
+                 default='list')
     opts, args = p.parse_args()
-    if False:   # TODO
-        pass
+    if len(args) == 2:
+        if opts.type == 'list':
+            is_list = True
+        else:
+            is_list = False
+        page_from = int(args[0])
+        page_to = int(args[1])
     else:
         p.print_help()
         sys.exit()
-    is_list, page_from, page_to = 1, 1, 8
     return is_list, page_from, page_to
 
 
+def change_list_order(list):
+    for i in range(0, len(list) // 2, 2):
+        j = len(list) - i
+        list[i], list[j - 2] = list[j - 2], list[i]
+        list[i + 1], list[j - 1] = list[j - 1], list[i + 1]
+    return list
+
+
 def create_book(page_from, page_to):
-    # TODO: Переделать
     pages_even = []
     pages_odd = []
-    for i in range(page_from, page_to + 1, 4):
-        print(i)
+    pages_count = page_to - page_from + 1
+    if pages_count % 4 != 0:
+        pages_count += (4 - (pages_count % 4))
+        page_to = page_from + pages_count - 1
+    for i in range(0, pages_count, 4):
         pages_even.append(page_to - i)
         pages_even.append(page_from + i)
         pages_odd.append(page_from + i + 1)
         pages_odd.append(page_to - i - 1)
-    #for i in range(0, len(pages_odd) // 2, 2):
-        #j = len(pages_odd) - i
-        #pages_odd[i], pages_odd[j - 2] = pages_odd[j - 2], pages_odd[i]
-        #pages_odd[i + 1], pages_odd[j - 1] = pages_odd[j - 1], pages_odd[i + 1]
-    return pages_even, pages_odd
+    return pages_even, change_list_order(pages_odd)
 
 
 def create_list(page_from, page_to):
@@ -61,11 +75,7 @@ def create_list(page_from, page_to):
         pages_even.append(i + 1)
         pages_odd.append(i + 2)
         pages_odd.append(i + 3)
-    for i in range(0, len(pages_odd) // 2, 2):
-        j = len(pages_odd) - i
-        pages_odd[i], pages_odd[j - 2] = pages_odd[j - 2], pages_odd[i]
-        pages_odd[i + 1], pages_odd[j - 1] = pages_odd[j - 1], pages_odd[i + 1]
-    return pages_even, pages_odd
+    return pages_even, change_list_order(pages_odd)
 
 
 if __name__ == '__main__':
@@ -74,4 +84,5 @@ if __name__ == '__main__':
         pages_even, pages_odd = create_list(page_from, page_to)
     else:
         pages_even, pages_odd = create_book(page_from, page_to)
-    print('Нечетные листы:\n%s\nЧетные листы:\n%s' % (pages_even, pages_odd))
+    print('Нечетные страницы:\n%s\nЧетные страницы:\n%s'\
+          % (pages_even, pages_odd))
