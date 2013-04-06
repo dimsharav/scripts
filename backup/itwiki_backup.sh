59af3c1613ccaed3764ad94f70e3185c6a87ddb2
Dmitriy Sharavyov <dimsharav@gmail.com>
2013-04-06 16:38:54 +0600

    Бэкап данных dokuwiki

---
+++ backup/itwiki_backup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Бэкап dokuwiki с mars.prk.local
+# ===============================
+#
+# Зависимости:
+#   - sshfs
+#   - настроенная авторизация по ключам
+
+REMOTE_SERVER="192.168.0.4"
+REMOTE_DIR="/var/data"
+MOUNT_DIR="/media/net/mars/www"
+BACKUP_DIR="/media/net/uranus/e/backup/mars"
+BACKUP_DATE=$(date +%Y-%b-%d--%k_%M_%S)
+
+sshfs $REMOTE_SERVER:$REMOTE_DIR $MOUNT_DIR
+
+tar zcf $BACKUP_DIR/itwiki.$BACKUP_DATE.tar.gz $MOUNT_DIR/dokuwiki/
