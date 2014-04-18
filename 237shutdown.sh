92aaca4cd4b53095d54b26e22d352f141ffa625d
Dmitriy Sharavyov <dimsharav@gmail.com>
2014-04-18 19:26:06 +0600

    Не работало в одну строчку

--- 237shutdown.sh
+++ 237shutdown.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-for ((a = 102; a <= 114; a++)); do; ssh 192.168.0.$a -t sudo shutdown -h now; done
+for ((a = 102; a <= 114; a++))
+do
+    ssh 192.168.0.$a -t sudo shutdown -h now
+done
