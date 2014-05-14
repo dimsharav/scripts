2346bafb0ddab48b7e9081c8d0037b465c8afb67
Dmitriy Sharavyov <dimsharav@gmail.com>
2014-05-14 16:54:03 +0600

    nethack 'cheat' :)

---
+++ nhsave.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cp /home/dimsharav/backup/1000dimsharav.gz /var/games/nethack/save/
+chown dimsharav:games /var/games/nethack/save/1000dimsharav.gz
+chmod g+w /var/games/nethack/save/1000dimsharav.gz
