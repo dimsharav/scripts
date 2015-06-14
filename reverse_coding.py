0aade49b2aefcf0290327a3e17568554016668ba
Dmitriy Sharavyov <dimsharav@gmail.com>
2015-06-14 13:26:42 +0500

    Добавлен скрипт перевода текста, набранного в неправильной раскладке

---
+++ reverse_coding.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+'''
+Исправление ошибки раскладки
+'''
+
+letters_in = ("qwertyuiop[]asdfghjkl;'zxcvbnm,./QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>?@#$^&")
+letters_out = ("йцукенгшщзхъфывапролджэячсмитьбю.ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,\"№;:?")
+
+str_in = input('Введите строку на английской раскладке: ')
+str_out = ''
+
+for letter in str_in:
+    if letter in letters_in:
+        i = letters_in.find(letter)
+        str_out += letters_out[i]
+    else:
+        str_out += letter
+
+print(str_out)
