ceabb44581463f5bd21cbf7965195c6309740876
Dmitriy Sharavyov <dimsharav@gmail.com>
2015-06-14 21:25:51 +0500

    reverse_layout: вместо поиска подстроки словарь

--- reverse_layout.py
+++ reverse_layout.py
@@ -3,16 +3,16 @@
 Исправление ошибки раскладки
 '''
 
-letters_in = ("`qwertyuiop[]asdfghjkl;'zxcvbnm,./~QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>?@#$^&")
-letters_out = ("ёйцукенгшщзхъфывапролджэячсмитьбю.ЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,\"№;:?")
+letters_eng = ("`qwertyuiop[]asdfghjkl;'zxcvbnm,./~QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>?@#$^&")
+letters_rus = ("ёйцукенгшщзхъфывапролджэячсмитьбю.ЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,\"№;:?")
+letters = dict(zip(letters_eng, letters_rus))
 
 str_in = input('Введите строку на английской раскладке: ')
 str_out = ''
 
 for letter in str_in:
-    if letter in letters_in:
-        i = letters_in.find(letter)
-        str_out += letters_out[i]
+    if letter in letters_eng:
+        str_out += letters[letter]
     else:
         str_out += letter
 
