02147e27fc81880c72bc501da410414f26efbb99
Dmitriy Sharavyov <dimsharav@gmail.com>
2016-03-01 01:29:44 +0500

    compress-pdf.sh теперь работает с пробелами в именах файлов

--- compress-pdf.sh
+++ compress-pdf.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-mkdir output
+echo Compress arguments...
+echo For filenames with spaces use "*.pdf"
+echo " "
+
+compressed_dir="compressed"
+mkdir $compressed_dir
 for file in $*; do
-    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=output/$file $file
+    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$compressed_dir/"$file" "$file"
 done
